6d4d49b943a8172610db0a60bb197a5a12c9cc89
Daniel Braunstein <dbraunst@umich.edu>
2020-04-22 23:36:56 -0400

    Init Upload

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    private GameObject player = null;
+
+    //sound source index is as follows:
+    // 0: Left Wall Reflection  (-90)
+    // 1: Right Wall Refelction ( 90)
+    // 2: Left Front  (-45)
+    // 3: Right Front ( 45)
+    // 4: Left Back   (-135)
+    // 5: Right Back  ( 135)
+    public int soundSourceIndex = 0;
+
+    public float absWallDistance = 0.0f;
+
+    //v3 offset multiplier
+
+    private Vector3 offset = Vector3.zero;
+    // THOUGHTS: smarter to do offset / object managemenet via 
+    // 1: predetermined sources (e.g. L, R, U, D)
+    // 2: raycasts out in directions and flipping distance to wall
+    // 3: ???
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindWithTag("Player");
+        }
+
+        
+
+        setSourceOffset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        setSourceOffset();
+    }
+
+    // set location of soundsource away from wall based on player location
+    void setSourceOffset()
+    {
+        ///get player distance to wall based on 
+        absWallDistance = Mathf.Abs(player.GetComponent<Player>().distanceToWall[soundSourceIndex]);
+        
+        switch (soundSourceIndex)
+        {
+
+            case 0: // left
+                offset = (Vector3.left * absWallDistance) + player.GetComponent<Player>().hitLocations[0];
+                transform.position = offset;
+                break;
+            case 1: // right
+                offset = (Vector3.right * absWallDistance) + player.GetComponent<Player>().hitLocations[1];
+                transform.position = offset;
+                break;
+            case 2: // front left
+                offset = ((Vector3.left + Vector3.forward) * absWallDistance) + player.GetComponent<Player>().hitLocations[2];
+                transform.position = offset;
+                break;
+            case 3: // front right
+                offset = ((Vector3.right + Vector3.forward) * absWallDistance) + player.GetComponent<Player>().hitLocations[3];
+                transform.position = offset;
+                break;
+            case 4: // back left
+                offset = ((Vector3.left + Vector3.back) * absWallDistance) + player.GetComponent<Player>().hitLocations[4];
+                transform.position = offset;
+                break;
+            case 5: // back right
+                offset = ((Vector3.right + Vector3.back) * absWallDistance) + player.GetComponent<Player>().hitLocations[5];
+                transform.position = offset;
+                break;
+
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    static public int numSources = 6;
+
+    LayerMask mask = -2; //raycast ignores layer 2
+
+    private float maxRayDist = 100.0f;
+
+    public RaycastHit hit;
+
+    public float[] distanceToWall = new float[numSources];
+    public Vector3[] hitLocations = new Vector3[numSources];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Left
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, maxRayDist))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * hit.distance, Color.cyan);
+            distanceToWall[0] = hit.distance;
+            hitLocations[0] = hit.point;
+            //Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * 200, Color.white);
+            //Debug.Log("Did not Hit");
+        }
+
+        // Right
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, maxRayDist))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * hit.distance, Color.magenta);
+            distanceToWall[1] = hit.distance;
+            hitLocations[1] = hit.point;
+            //Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * 200, Color.white);
+            //Debug.Log("Did not Hit");
+        }
+
+        // Left Front
+        if (Physics.Raycast(transform.position, transform.TransformDirection(-1, 0, 1), out hit, maxRayDist))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(-1, 0, 1) * hit.distance, Color.cyan);
+            distanceToWall[2] = hit.distance;
+            hitLocations[2] = hit.point;
+            //Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(-1, 0, 1) * 200, Color.white);
+            //Debug.Log("Did not Hit");
+        }
+
+        // Right Front
+        if (Physics.Raycast(transform.position, transform.TransformDirection(1, 0, 1), out hit, maxRayDist))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(1, 0, 1) * hit.distance, Color.magenta);
+            distanceToWall[3] = hit.distance;
+            hitLocations[3] = hit.point;
+            //Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(1, 0, 1) * 200, Color.white);
+            //Debug.Log("Did not Hit");
+        }
+
+        // Left Back
+        if (Physics.Raycast(transform.position, transform.TransformDirection(-1, 0, -1), out hit, maxRayDist))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(-1, 0, -1) * hit.distance, Color.cyan);
+            distanceToWall[4] = hit.distance;
+            hitLocations[4] = hit.point;
+            //Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(-1, 0, -1) * 200, Color.white);
+            //Debug.Log("Did not Hit");
+        }
+
+        // Right Back
+        if (Physics.Raycast(transform.position, transform.TransformDirection(1, 0, -1), out hit, maxRayDist))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(1, 0, -1) * hit.distance, Color.magenta);
+            distanceToWall[5] = hit.distance;
+            hitLocations[5] = hit.point;
+            //Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(1, 0, -1) * 200, Color.white);
+            //Debug.Log("Did not Hit");
+        }
+    }
+}
