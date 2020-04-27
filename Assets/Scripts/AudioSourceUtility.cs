ab5fbf9c78b42c0caea3a11fd0f5fdc37f92811e
olCainlo <m.fisher.drums@googlemail.com>
2020-04-26 21:41:00 -0400

    Added an Audiosource utility script to randomize play points within a sound file and to stop audiosources from playing when player is outside a certain range. Also added audiosources and clips to LR and RR sources along with audiosource script.

---
+++ Assets/Scripts/AudioSourceUtility.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceUtility : MonoBehaviour
+{
+    AudioSource audiosource;
+    AudioClip clip;
+
+    public GameObject player;
+
+    float maxDistance;
+
+    void Start()
+    {
+        
+
+    }
+
+    void Update()
+    {
+        maxDistance = audiosource.maxDistance;
+
+        float distance = Vector3.Distance(player.transform.position, transform.position);
+
+        if (distance <= maxDistance && audiosource.isPlaying == false)
+        {
+            PlaySound();
+        }
+
+        if (distance > maxDistance)
+        {
+            audiosource.Stop();
+        }
+
+    }
+
+    void PlaySound()
+    {
+        clip = audiosource.clip;
+        audiosource.time = Random.Range(0f, clip.length);
+        audiosource.Play();
+    }
+}
