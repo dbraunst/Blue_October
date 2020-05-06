8a21e126ef16e0ec4b02f9f9119552064a399d3d
Daniel Braunstein <dbraunst@umich.edu>
2020-05-06 19:10:30 -0400

    Made movement physics-based
    Changed movement from direct translation/rotaiton settings to addforce() + addtorque(). Mass, Drag, + Angular drag currently set at 15, 1, 1, with movespeed set to 20 and turn to 5.
    
    Currently, rotating up/down is set to turnspeed / 2, but also X/Z rotation frozen + Y position so that gampleay function is disabled ATM.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float moveSpeed = 2.0f;
-    public float accelSpeed = 0.0f;
-    public float turnSpeed = 1.0f;
+    public float moveSpeed = 20.0f;
+    public float turnSpeed = 5.0f;
 
     public bool swapVerticalAxes = false;
     private int swapVertAxes = 1;
@@ -15,10 +14,16 @@ public class PlayerController : MonoBehaviour
     private float rotationY;
     private float rotationX;
 
+    private float lockPos = 0.0f;
+
+    private Rigidbody body;
+
     // Start is called before the first frame update
     void Start()
     {
         if (swapVerticalAxes) swapVertAxes = -1;
+
+        body = GetComponent<Rigidbody>();
     }
 
     // FixedUpdate is frame-rate independent (put physics stuff here)
@@ -27,18 +32,18 @@ public class PlayerController : MonoBehaviour
         //spacebar to move forward
         if (Input.GetKey(KeyCode.Space))
         {
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            //body.AddForce(Vector3.forward * 1.0f * moveSpeed * Time.deltaTime, ForceMode.Force );
+            body.AddForce(transform.forward * moveSpeed, ForceMode.Force);
         }
 
         // Get Input axes for movement controls
         // LR as normal, flip UP/DOWN controls bc submarine.
         // 'swapVerticalAxes' lets user switch this if they want
-        rotationX = Input.GetAxis("Vertical") * turnSpeed * swapVertAxes * Time.deltaTime;
-        rotationY = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+        body.AddTorque(transform.right * swapVertAxes * turnSpeed / 2 * Input.GetAxis("Vertical"), ForceMode.Force);
+        body.AddTorque(transform.up * turnSpeed * Input.GetAxis("Horizontal"), ForceMode.Force);
 
-        //rotate left/right relative to world, up down relative to self
-        transform.Rotate(0.0f, rotationY, 0.0f, Space.World); //Left/Right
-        transform.Rotate(rotationX, 0.0f, 0.0f, Space.Self); //Up/Down
+        //Lock Z rotation
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, lockPos);
     }
 
     // Update is called once per frame (put visual stuff here)
