478219f1bcc0316df75639c2f731d36951111306
olCainlo <m.fisher.drums@googlemail.com>
2020-05-05 23:32:31 -0400

    Engine reflection more directional; added sonar ping and sonar return wave based on distance; edited distance for engine reflection audiosources

---
+++ Assets/Scripts/Sonar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sonar : MonoBehaviour
+{
+    public AudioSource sonarPing;
+    AudioClip sonarPingClip;
+
+    public AudioSource sonarReturn;
+    AudioClip sonarReturnClip;
+
+
+    public GameObject player;
+
+    float timeDelay;
+    public float arbNumber;
+
+    void Start()
+    {
+        
+        
+    }
+
+    void Update()
+    {
+        timeDelay = Vector3.Distance(player.transform.position, this.transform.position)/50;
+
+        if (Input.GetButtonDown("Fire1") && sonarPing.isPlaying == false)
+        {
+            sonarPing.Play();
+            StartCoroutine(SonarReturn());
+        }
+           
+    }
+
+    IEnumerator SonarReturn()
+    {        
+        yield return new WaitForSeconds(timeDelay);
+        sonarReturn.Play();
+    }
+
+}
