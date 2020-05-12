21c6e09f337a03061eddcd637d56c90386227f85
Daniel Braunstein <dbraunst@umich.edu>
2020-05-12 01:51:57 -0400

    Checkpoints Working - not in Levels Yet
    Checkpoints system is working, basic how-to:
    - create empty Checkpoint GameObjects, with collider boxes (As Trigger: true), assign 'CheckPoint' Tag and add to 'Ignore Raycast' layer. Place collider boxes in space where you want them to trigger.
    
    on PLAYER:
     - add CheckpointHandler.cs, will auto-load objective GO if you don't drag it in
    on OBJECTIVE:
     - added functionality to 'sceneManagement.cs' , will auto-load Player GO if you don't drag it in
     -  set size of 'Check Points' array, drag (in-order) checkpoint GameObjects
    
    Effect:
    calculates distance from next checkpoint thru remaining to objective, offsets the objective GO by the 'remaining distance' opposite the player so delay + attenuation is still working perfectly, when player hits last checkpoint, objective snaps back to it's original/intended location.
    
    TODO: Implement as level design in levels 2+3

---
+++ Assets/CheckpointHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointHandler : MonoBehaviour
+{
+
+    public GameObject objective;
+
+    private void Awake()
+    {
+        if (objective == null)
+        {
+            objective = GameObject.FindGameObjectWithTag("Objective");
+        }
+    }
+
+    //If we hit the checkpoints as specified by the objective, call the
+    //  playerTriggerEnter script from objective's SceneManagement script
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("CheckPoint"))
+        {
+            Debug.Log("thru checkpoint trigger");
+
+            objective.GetComponent<SceneManagement>().playerTriggerEnter(other);
+        }
+    }
+}
--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -5,6 +5,97 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
+    public GameObject player;
+
+    public GameObject[] checkPoints;
+    public int numCheckPointsHit = 0;
+
+    //intial/end/intended position of objective
+    public Vector3 finalPos;
+
+    public float totalDistance = 0.0f;
+
+    private Vector3 playerToCheckpoint;
+    private Vector3 dir;
+
+    void Awake()
+    {
+        //set final pos based on set position in scene
+        finalPos = transform.position;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Start()
+    {
+        if (checkPoints[0] == null)
+        {
+            Debug.Log("Error: No Checkpoints Set");
+        }
+
+        totalDistance = getRemainingDistance(numCheckPointsHit);
+    }
+
+    //calculates the distance from each remaining checkpoint to the next until the
+    //objective, returning 0 once the last checkpoint has been hit
+    float getRemainingDistance(int _numCheckPointsHit)
+    {
+        float remainingDist = 0.0f;
+
+        //set total distance from player to objective
+        for (int i = _numCheckPointsHit; i < checkPoints.Length - 1; i++)
+        {
+            remainingDist += Vector3.Distance(checkPoints[i].transform.position,
+                    checkPoints[i+1].transform.position);
+        }
+        //lastly, add distance from last checkpoint to objective
+        if (numCheckPointsHit < checkPoints.Length)
+        {
+            remainingDist += Vector3.Distance(checkPoints[checkPoints.Length - 1].transform.position,
+                    finalPos);
+        }
+
+        return remainingDist;
+    }
+
+
+    void Update()
+    {
+        //get total distance offset of objective to next checkpoint
+        totalDistance = getRemainingDistance(numCheckPointsHit);
+
+        //pivot position around the next checkpoint while remaining distanced from player
+        if (numCheckPointsHit < checkPoints.Length)
+        {
+            //get vector3 direction between player
+            dir = checkPoints[numCheckPointsHit].transform.position
+                  - player.transform.position;
+            dir.y = 0.0f; //make offset only on x and Z planes
+
+            transform.position = checkPoints[numCheckPointsHit].transform.position + (dir.normalized * totalDistance);
+            transform.position.Set(transform.position.x, finalPos.y, transform.position.z);
+        }
+        else if (numCheckPointsHit == checkPoints.Length)
+        {
+            transform.position = finalPos;
+        }
+    }
+
+
+    //This function is called from CheckPointHandler.cs when player enters a
+    //  trigger with the tag 'checkpoint'
+    public void playerTriggerEnter(Collider other)
+    {
+        //if we still have checkpoints left to hit
+        if (numCheckPointsHit < checkPoints.Length)
+        {
+            //AND if the object we hit was the next checkpoint
+            if (other.gameObject == checkPoints[numCheckPointsHit])
+            {
+                //increment the checkpoints hit
+                numCheckPointsHit++;
+            }
+        }
+    }
 
 
     public void LoadNextLevel()
@@ -18,5 +109,6 @@ public class SceneManagement : MonoBehaviour
     {
         LoadNextLevel();
     }
+
 
 }
